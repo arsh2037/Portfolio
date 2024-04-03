40eabdbab76cb203fabd006838e78121d5d922f8
arsh2037 <arshhasan68@gmail.com>
2024-04-03 22:36:13 +0930

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="blink">Coming Soon</h1>
+      <div className="card">
+        <button onClick={() => window.location.href = "https://drive.google.com/file/d/1wsMxQjJnOPXbQRm7oDVJX2pi51Mb6BFm/view?usp=sharing"}>
+          Checkout my CV
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
