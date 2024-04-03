d05af77f08c028185ce8019bf8496b44bd85fa42
arsh2037 <arshhasan68@gmail.com>
2024-04-03 23:05:38 +0930

    Button Bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
         <button onClick={() => window.location.href = "https://drive.google.com/file/d/1wsMxQjJnOPXbQRm7oDVJX2pi51Mb6BFm/view?usp=sharing"}>
           Checkout my CV
         </button>
-        <button onClick={() => window.location.href = "hhttps://www.linkedin.com/in/arsh-hasan/"}>
+        <button onClick={() => window.location.href = "https://www.linkedin.com/in/arsh-hasan/"}>
           Connect with me on LinkedIn
         </button>
       </div>
